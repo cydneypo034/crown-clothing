632933824b5ad9c0db57c4127eb4b6f734eeda55
Cydney Pollard <sincerelycydney087@gmail.com>
2019-12-07 15:41:44 -0500

    added cart redux to cart icon and cart styling

--- src/components/header-component/header.component.jsx
+++ src/components/header-component/header.component.jsx
@@ -7,6 +7,7 @@ import { auth } from '../../firebase/firebase.utils';
 
 import {ReactComponent as Logo} from '../../assets/crown.svg'
 import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import './header.styles.scss';
 
 const Header = ({ currentUser }) => (
@@ -30,6 +31,7 @@ const Header = ({ currentUser }) => (
         }
         <CartIcon />
         </div>
+        <CartDropdown />
     </div>
 );
 
