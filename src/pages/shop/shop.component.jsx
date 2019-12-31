cebd013ae177145c9f4dae902416b75d27bfc1b4
Cydney Pollard <sincerelycydney087@gmail.com>
2019-12-31 08:43:09 -0500

    creating collection component for each category

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import ShopData from './shop.data';
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
 import CollectionPreview from '../../components/preview-collection/preview-collection.component'
 
-class ShopPage extends React.Component {
-    constructor(props){
-        super(props);
+import { selectCollections } from "../../redux/shop/shop.selectors";
 
-        this.state = {
-            collections: ShopData
-        }
-    }
-    render() {
-        const {collections} = this.state;
-        return (
-            <div className='shop-page'>
+const ShopPage = ({ collections }) => (
+    <div className='shop-page'>
                 {collections.map(({id, ...otherCollectionProps}) => (
                     <CollectionPreview key={id} {...otherCollectionProps}/>
                 ))}
             </div>
-        )
-    }
-}
+            )
 
-export default ShopPage;
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+});
+export default connect(mapStateToProps)(ShopPage);
