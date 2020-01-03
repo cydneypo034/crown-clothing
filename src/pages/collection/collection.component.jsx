6549858888a262c36cf69f94c07f33925eb5e80c
Cydney Pollard <sincerelycydney087@gmail.com>
2020-01-03 10:45:47 -0500

    stored list of items as objects instead of arrays by data normalization and styled collection pages

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { connect } from 'react-redux';
 
-import { selectCollection, selectCollections } from "../../redux/shop/shop.selectors";
+import { selectCollection } from "../../redux/shop/shop.selectors";
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
@@ -10,10 +10,17 @@ import './collection.styles.scss';
 
 
 const CollectionPage = ({ collection }) => {
-    console.log(collection);
+    const {title, items} = collection;
     return (
     <div className='collection-page'>
-        <h2>Collection PAGE</h2>
+        <h2 className='title'>{title}</h2>
+        <div className='items'>
+            {
+                items.map(item => (
+                    <CollectionItem key={item.id} item={item} />
+                ))
+            }
+        </div>
     </div>
 )};
 
